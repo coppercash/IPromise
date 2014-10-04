883ef2babb20a5cf48b10c8344f148be44c04854
coppercash <coderdreamer@gmail.com>
2014-10-04 08:56:58 +0800

    Make Promise implement some thanable functions

--- CCOPromise/Promise.swift
+++ CCOPromise/Promise.swift
@@ -8,23 +8,49 @@
 
 import Foundation
 
-public class Promise<V>: APlusPromise
+public class Promise<V>: Thenable
 {
     // MARK: - Type
 
-    typealias ThenType = Promise
-    typealias ValueType = V
-    typealias ReasonType = NSError?
+    public typealias ThenType = Promise
+    public typealias ValueType = V
+    public typealias ReasonType = NSError?
+    public typealias ReturnType = Promise
 
     public typealias Resolution = (value: V) -> Any?
     public typealias Rejection = (reason: NSError?) -> Any?
     public typealias Resovler = (value: V) -> Void
     public typealias Rejector = (reason: NSError?) -> Void
 
-    public var value: V
+    public var value: V?
     
     // MARK: - Initializers
+    public init()
+    {
+        //self.value = nil
+    }
+    
+    public init(value: V)
+    {
+    }
+    
+    convenience
+    public init<V, T: Thenable where T.ValueType == V, T.ReasonType == Optional<Any>, T.ReturnType == Optional<Any>>(thenable: T)
+    {
+        self.init()
+        thenable.then(
+            onFulfilled: { (value) -> Any? in
 
+                return nil
+            },
+            onRejected: { (reason) -> Any? in
+
+                return nil
+            }
+        )
+    }
+    
+/*
     required
     public init(resovler: (resolve: Resovler, reject: Rejector) -> Void)
     {
@@ -85,7 +111,7 @@ public class Promise<V>: APlusPromise
             }
         )
     }
-
+*/
     
     // MARK: - Public APIs
     /*
@@ -97,7 +123,7 @@ public class Promise<V>: APlusPromise
         ) as Promise<N>
     }
     */
-    
+    /*
     public func catch(onRejected: Rejection) -> Promise<Any?>
     {
         return self.then(
@@ -105,13 +131,35 @@ public class Promise<V>: APlusPromise
             onRejected: onRejected
         )
     }
-
+*/
     
     // MARK: - Thenable
 
+    public func then<T>(
+        onFulfilled: Optional<(value: V) -> Promise<T>> = nil,
+        onRejected: Optional<(reason: NSError?) -> Promise<T>> = nil
+        ) -> Promise<T> {
+        return Promise<T>()
+    }
     
+    public func then<T>(
+        onFulfilled: Optional<(value: V) -> T> = nil,
+        onRejected: Optional<(reason: NSError?) -> T> = nil
+        ) -> Promise<T> {
+        return Promise<T>()
+    }
+    
+    public func then<V>(
+        onFulfilled: Optional<(value: V) -> Void> = nil,
+        onRejected: Optional<(reason: NSError?) -> Void> = nil
+        ) -> Promise<V> {
+            return Promise<V>()
+    }
+
+    /*
     public func then(onFulfilled: Resolution? = nil, onRejected: Rejection? = nil) -> Promise<Any?>
     {
         return super.then(onFulfilled: onFulfilled, onRejected: onRejected) as Promise<Any?>
     }
+*/
 }
