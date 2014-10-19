b6a1fd16301c79343cb35c769576257e1b77f4c5
coppercash <coderdreamer@gmail.com>
2014-10-19 12:16:36 +0800

    Protective commit

--- IPromiseTests/ReadMeTests.swift
+++ IPromiseTests/ReadMeTests.swift
@@ -13,30 +13,54 @@ import IPromise
 class ReadMeTests: XCTestCase {
 
     func test_typeSafe() {
-        
-        func answerToEverything() -> Promise<Int> {
-            return Promise(value: 42);
+
+        func answerToEverthing() -> Promise<Int> {
+            return Promise(value: 42)
         }
         
-        answerToEverything()
-            .then(
-                onFulfilled: { (value: Int) -> Bool in
-                    return value == 42
-                },
-                onRejected: { (reason: NSError) -> Bool in
-                    return false
-            })
-            .then { (value: Bool) -> Promise<String> in
+        let promise: Promise<Int> = answerToEverthing();
+        
+        promise
+            .then { (value: Int) -> Bool in
+                return value == 42
+            }.then { (value: Bool) -> Promise<String> in
                 return value ?
                     Promise(value: "I knew it!") :
                     Promise(reason: NSError())
+            }.then { (value: String) -> Void in
+                println(value.stringByAppendingString(" Oh yeah!"))
             }
-            .then { (value: String) -> String in
-                return value.stringByAppendingString(" Oh yeah!")
-            }
-            .catch { (reason: NSError) -> Void in
-                println("This won't be invoked, coz the answer must be 42")
-        }
     }
 
+    func test_TypeFree() {
+        
+        func answerToUniverse() -> APlusPromise {
+            return APlusPromise(value: 42);
+        }
+        
+        let typeFreePromise: APlusPromise = answerToUniverse()
+        
+        typeFreePromise.then(
+            onFulfilled: { (value: Any?) -> Any? in
+                let isItStill42 = (value as Int) == 42
+                return nil;
+            },
+            onRejected: { (reason: Any?) -> Any? in
+                
+                return nil;
+            }
+        );
+        
+        let fromTypeFree: Promise<Any?> = Promise(vagueThenable: typeFreePromise)
+        let fromTypeSafe: APlusPromise = APlusPromise(promise: fromTypeFree)
+    }
+    
+    func test_aggregate() {
+        
+    }
+    
+    func test_chain() {
+        
+    }
+    
 }
